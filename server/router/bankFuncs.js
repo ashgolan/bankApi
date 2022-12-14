3be58c8ca0c54703a99dcfb8a9cecc374344e33a
ashgolan <ashgolan@hotmail.com>
2022-12-14 16:08:50 +0200

    test

--- server/router/bankFuncs.js
+++ server/router/bankFuncs.js
@@ -17,12 +17,12 @@ import {
 export const bankRouter = router();
 
 bankRouter.post("/users", addNewClient);
-bankRouter.get("/users/", getClients);
+bankRouter.get("/users", getClients);
 bankRouter.get("/users/:id", getSpesificClient);
 bankRouter.put("/:id", updateClient);
 bankRouter.delete("/:id", deleteClient);
 //---------------------------------------------------
-bankRouter.get("/accounts/", getAccounts);
+bankRouter.get("/accounts", getAccounts);
 bankRouter.get("/accounts/deposit/:id/:amount", depositCash);
 bankRouter.post("/accounts/addAccount/:owner/:credit", addNewAccount);
 bankRouter.put("/accounts/updateAccount/:id/:newCredit", updateAccount);
