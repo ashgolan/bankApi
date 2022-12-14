648471953a45adf23696e68a2cec04e64a7cffdf
ashgolan <ashgolan@hotmail.com>
2022-12-14 16:10:21 +0200

    test

--- server/router/index.router.js
+++ server/router/index.router.js
@@ -1,4 +1,4 @@
 import router from "express";
 import { bankRouter } from "./bankFuncs.js";
 export const indexRoute = router();
-indexRoute.use("bank", bankRouter);
+indexRoute.use("/bank", bankRouter);
