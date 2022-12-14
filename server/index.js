5403789eb017f1587db5514eedaf4db4481df9bf
ashgolan <ashgolan@hotmail.com>
2022-12-14 15:21:35 +0200

    fix to render

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ import { bankRouter } from "./router/bankFuncs.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.use("/api", bankRouter);
 app.listen(PORT, () => {
   console.log("Listining to port 5000");
