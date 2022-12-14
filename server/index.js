1245cc860a48987447b4ca6f7f4400f9f20dde39
ashgolan <ashgolan@hotmail.com>
2022-12-14 15:48:27 +0200

    remove .git

--- server/index.js
+++ server/index.js
@@ -8,4 +8,5 @@ const PORT = process.env.PORT || 5000;
 app.use("/api", bankRouter);
 app.listen(PORT, () => {
   console.log("Listining to port 5000");
+  // console.log("Listining to port 5000");
 });
